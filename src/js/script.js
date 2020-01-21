1607ab91ab5b58794cb4858d73f33a1ecaac387e
arsenyNebozhenko <nebozhenko.arseny@gmail.com>
2020-01-21 07:16:40 +0300

    added carousel plugin (had to extend it)

--- src/js/script.js
+++ src/js/script.js
@@ -37,3 +37,62 @@
     }
   })
 }
+
+// Carousel
+{
+  // My own plugin
+  Siema.prototype.slideToCurrent = function(enableTransition) {
+    const buttons = this.selector.parentElement.querySelector('.testimonials-carousel__buttons').children
+    this.activateDots(buttons, this.currentSlide)
+
+    const currentSlide = this.config.loop ? this.currentSlide + this.perPage : this.currentSlide;
+    const offset = (this.config.rtl ? 1 : -1) * currentSlide * (this.selectorWidth / this.perPage);
+
+    if (enableTransition) {
+      // This one is tricky, I know but this is a perfect explanation:
+      // https://youtu.be/cCOL7MC4Pl0
+      requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+          this.enableTransition();
+          this.sliderFrame.style[this.transformProperty] = `translate3d(${offset}px, 0, 0)`;
+        });
+      });
+    }
+    else {
+      this.sliderFrame.style[this.transformProperty] = `translate3d(${offset}px, 0, 0)`;
+    }
+  }
+
+  Siema.prototype.addPagination = function() {
+    const buttonsWrapper = this.selector.parentElement.querySelector('.testimonials-carousel__buttons')
+
+    const buttons = this.innerElements.map((element, i) => {
+      const button = document.createElement('button');
+      const className = i === 0 ? 'testimonials-carousel__buttons-button testimonials-carousel__buttons-button--active' : 'testimonials-carousel__buttons-button'
+
+      button.className = className
+      
+      button.addEventListener('click', () => this.goTo(i))
+      
+      return button
+    })
+
+    buttons.forEach(button => {
+      buttonsWrapper.appendChild(button)
+    })
+  }
+
+  Siema.prototype.activateDots = function(buttons, index) {
+    if (buttons.length) {
+      Array.from(buttons).forEach(button => button.classList.remove('testimonials-carousel__buttons-button--active'))
+      buttons[index].classList.add('testimonials-carousel__buttons-button--active')
+    }
+  }
+
+  // Initialization
+  const carousel = new Siema({
+    selector: '.testimonials-carousel__list'
+  })
+
+  carousel.addPagination();
+}
