bb8b64799f6e55d334dbd814fb4e9d4bb57cc5a8
arsenyNebozhenko <nebozhenko.arseny@gmail.com>
2020-01-21 03:37:57 +0300

    added header on scroll functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
+// Header onscroll behavior
+{
+  const header = document.querySelector('#header')
+  const home = document.querySelector('#home')
 
+  const handleScroll = () => {
+    const headerPositionFromTop = header.getBoundingClientRect().top
+
+    if (headerPositionFromTop <= 0 && !header.classList.contains('section-header--active')) {
+      header.classList.add('section-header--active')
+    } else if (home.clientHeight - pageYOffset >= header.clientHeight) {
+      header.classList.remove('section-header--active')
+    }
+  }
+
+  window.addEventListener('scroll', handleScroll)
+}
